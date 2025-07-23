af1d0e3c3f6b8896be4c3cf984ef4e32bc424be3
Infant Derrick Gnanasusairaj <rohanshanbhag2001@gmail.com>
2025-07-22 20:02:36 -0400

    Update description

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -1,78 +1,165 @@
 public class Lab1 {
 
-    // 1. Say Hello
+    /**
+     * Returns a greeting message for the given name.
+     *
+     * @param name the name to greet
+     * @return a greeting message in the format "Hello, [name]!"
+     */
     public static String sayHello(String name) {
         return null;
     }
 
-    // 2. Add two numbers (binary operator)
+    /**
+     * Adds two integers and returns the result.
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return the sum of a and b
+     */
     public static int add(int a, int b) {
         return 0;
     }
 
-    // 3. Negate a number (unary operator)
+    /**
+     * Returns the negation of the given number.
+     *
+     * @param num the number to negate
+     * @return the negative value of num
+     */
     public static int negate(int num) {
         return 0;
     }
 
-    // 4. Return the greater number (ternary operator)
+    /**
+     * Returns the greater of two numbers. If equal, returns either.
+     *
+     * @param a first number
+     * @param b second number
+     * @return the greater of a and b
+     */
     public static int greater(int a, int b) {
         return 0;
     }
 
-    // 5. Calculate rectangle area
+    /**
+     * Calculates the area of a rectangle using the given dimensions.
+     *
+     * @param length the length of the rectangle
+     * @param width the width of the rectangle
+     * @return the area of the rectangle
+     */
     public static int rectangleArea(int length, int width) {
         return 0;
     }
 
-    // 6. Grade category
+    /**
+     * Returns a letter grade (A, B, C, F) based on the numerical score.
+     * A: 90-100, B: 80-89, C: 70-79, F: below 70
+     *
+     * @param score the numeric score
+     * @return the grade category as a string
+     */
     public static String gradeCategory(int score) {
         return null;
     }
 
-    // 7. Even or Odd
+    /**
+     * Determines whether a number is even.
+     *
+     * @param number the number to check
+     * @return true if the number is even, false if odd
+     */
     public static boolean isEven(int number) {
         return false;
     }
 
-    // 8. Number triangle pattern
+    /**
+     * Prints a number triangle pattern of size n to the console.
+     * For example, n = 3 prints:
+     * 1
+     * 12
+     * 123
+     *
+     * @param n the height of the triangle
+     */
     public static void numberTriangle(int n) {
     }
 
-    // 9. Sum from 1 to n
+    /**
+     * Computes the sum of all numbers from 1 to n.
+     *
+     * @param n the upper bound
+     * @return the sum from 1 to n
+     * @throws IllegalArgumentException if n < 0
+     */
     public static int sumToN(int n) {
         return 0;
     }
 
-    // 10. FizzBuzz
+    /**
+     * Prints the FizzBuzz sequence up to n.
+     * - "Fizz" for multiples of 3
+     * - "Buzz" for multiples of 5
+     * - "FizzBuzz" for multiples of both 3 and 5
+     *
+     * @param n the upper bound
+     */
     public static void fizzBuzz(int n) {
     }
 
-    // 11. Reverse number
+    /**
+     * Reverses the digits of an integer.
+     *
+     * @param num the number to reverse
+     * @return the reversed number (maintains sign)
+     */
     public static int reverseNumber(int num) {
         return 0;
     }
 
-    // 12. Check prime
+    /**
+     * Determines if a number is prime.
+     *
+     * @param num the number to check
+     * @return true if prime, false otherwise
+     */
     public static boolean isPrime(int num) {
         return false;
     }
 
-    // 13. Count digits
+    /**
+     * Counts the number of digits in an integer.
+     *
+     * @param num the number to count digits of
+     * @return the number of digits
+     */
     public static int countDigits(int num) {
         return 0;
     }
 
-    // 14. Palindrome
+    /**
+     * Checks if a string is a palindrome (reads the same forwards and backwards).
+     *
+     * @param word the string to check
+     * @return true if palindrome, false otherwise
+     */
     public static boolean isPalindrome(String word) {
         return false;
     }
 
-    // 15. Factorial (recursive)
+    /**
+     * Calculates the factorial of a non-negative integer using recursion.
+     *
+     * @param n the number to compute factorial for
+     * @return the factorial of n
+     * @throws IllegalArgumentException if n < 0
+     */
     public static int factorial(int n) {
         return 0;
     }
 
     public static void main(String[] args) {
+        // Optional: Add manual testing or demonstration here
     }
 }
